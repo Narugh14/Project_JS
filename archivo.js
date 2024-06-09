221965e36bb9cfa1655b422773cd0591e91f3af5
Narugh14 <39018795+Narugh14@users.noreply.github.com>
2024-06-09 13:17:09 -0600

    Operadores de asignacion
    Modificar script en JS y observar comportamiento de los operadores.

--- archivo.js
+++ archivo.js
@@ -1,8 +1,37 @@
 
 
+//Operadores de asignacion, incremento y decremento
 
-var nombre = 'Hello2';
 
-var Nombre = 'Hello';
+let a = 1;
 
-document.write(nombre);
+document.write('Valor de a: ', a );
+document.write('<br>');
+
+a++; //Aumenta valor en 1
+document.write('Valor de a: ', a );
+document.write('<br>');
+
+a += 1;//Aumento variable
+document.write('Valor de a: ', a );
+document.write('<br>');
+
+a-- ; //Decremento de 1
+document.write('Valor de a: ', a );
+document.write('<br>');
+
+a -= 3; //Decremento de 3
+document.write('Valor de a: ', a );
+document.write('<br>');
+
+a *= 3; //Multiplicar por 2
+document.write('Valor de a: ', a );
+document.write('<br>');
+
+a /= 4; //Dividir por 4
+document.write('Valor de a: ', a );
+document.write('<br>');
+
+a **= 5; //Potenciador por 5
+document.write('Valor de a: ', a );
+document.write('<br>');
