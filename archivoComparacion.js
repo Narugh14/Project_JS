f6eb7338b6ad23e7a6a2aea5e6c013582b560180
Narugh14 <39018795+Narugh14@users.noreply.github.com>
2024-06-09 13:26:31 -0600

    Operadores de comparacion

---
+++ archivoComparacion.js
@@ -0,0 +1,25 @@
+
+
+let valor1, valor2;
+
+valor1 = 20;
+valor2 = 30;
+
+//Mayor que valor2
+document.write(valor1 > valor2 );
+document.write('<br>');
+//Menor que valor2
+document.write(valor1 < valor2 );
+document.write('<br>');
+//Igual que valor2
+document.write(valor1 == valor2);
+document.write('<br>');
+//Diferente a valor2
+document.write(valor1 != valor2);
+document.write('<br>');
+ //Menor o Igual a valor2
+document.write(valor1 <= valor2);
+document.write('<br>');
+//Mayor o Igual a valor2
+document.write(valor1 >= valor2);
+document.write('<br>');
